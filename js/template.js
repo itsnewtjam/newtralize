bc5b2a363c56b832024aed02a0e9cb68df60df67
James <78121730+itsnewtjam@users.noreply.github.com>
2023-12-13 13:39:01 -0500

    add option for navigation animation time (#24)

--- js/template.js
+++ js/template.js
@@ -3,15 +3,20 @@ function toggleMenu() {
   let menu = document.getElementById("primary-navigation");
   let overlay = document.querySelector("#nav-button + .menu-overlay");
   let isOpen = button.getAttribute("aria-expanded") == "true";
+  let navTime = newtralizeData['nav_time'] ?? 0;
   if (isOpen) {
     window.removeEventListener("keydown", handleMenuKeyDown);
     button.setAttribute("aria-expanded", "false");
-    menu.dataset.state = "closed";
-    overlay.dataset.state = "closed";
+    menu.dataset.state = "closing";
+    overlay.dataset.state = "closing";
     document.documentElement.style.overflow = "";
     document.body.style.position = "";
     document.body.style.inset = "";
     document.body.style.pointerEvents = "";
+    setTimeout(() => {
+      menu.dataset.state = "closed";
+      overlay.dataset.state = "closed";
+    }, navTime);
   } else {
     window.addEventListener("keydown", handleMenuKeyDown);
     let focusable = menu.querySelectorAll(
@@ -19,12 +24,16 @@ function toggleMenu() {
     );
     focusable[0].focus();
     button.setAttribute("aria-expanded", "true");
-    menu.dataset.state = "open";
-    overlay.dataset.state = "open";
+    menu.dataset.state = "opening";
+    overlay.dataset.state = "opening";
     document.documentElement.style.overflow = "hidden";
     document.body.style.position = "fixed";
     document.body.style.inset = "0";
     document.body.style.pointerEvents = "none";
+    setTimeout(() => {
+      menu.dataset.state = "open";
+      overlay.dataset.state = "open";
+    }, navTime);
   }
 }
 
